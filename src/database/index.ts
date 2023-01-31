4758c392a6f78102a64b64b373976e591cbdc531
RenanLira <renanalmeida2015.ra@gmail.com>
2023-01-31 13:41:08 -0300

    database config

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,17 +1,21 @@
 import { useMultiFileAuthState } from "@adiwajshing/baileys"
 import BaileysBottle from 'baileys-bottle'
 import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config()
+dotenv.config({path: path.join(__dirname, '..', '..', '.env') })
+
+console.log(process.env.PORT)
 
 async function bootstrapDataBase() {
     
     const database = await BaileysBottle.init({
         type: 'postgres',
+        url: process.env.DATABASE_URL,
         database: process.env.DATABASE,
         port: Number(process.env.PORT),
         host: process.env.HOST,
-        username: process.env.USERNAME,
+        username: process.env.USERNAME_DATABASE,
         password: process.env.PASSWORD
     })
     
